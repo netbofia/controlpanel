0e36225abe43393f605af3d19521beb49d154fbd
Bruno Costa <netbofia@hotmail.com>
2018-09-30 15:58:51 +0100

    Adds parsing of all host logs to generate multiple heatmaps Fixes #1

--- components/parsing/buildHeatmapMatrix.js
+++ components/parsing/buildHeatmapMatrix.js
@@ -1,51 +1,69 @@
 var data=require('./getLog')
+var glob=require('glob')
+var path=require('path')
 
-var file="/home/brunocosta/Downloads/git/controlpanel/components/parsing/../hosts/srna-portal.biodata.pt.log"
+const host="biodata.pt"
 const days=7
 const hours=24
+const hosts=getHostNames()
 
-exports.matrix=data.getMatrix(file).then((res)=>{
 
-	var datelist=Object.keys(res).sort()
-	var numOfdates=datelist.length
-	var matrix={}
-	matrix.data=generateMatrix(days,hours)
-	labels={
-	  	rows:[],
-	  	cols:[]
-	}
+exports.matrices=function(){
+	return new Promise(function(resolve,reject){
+		try{
+			result={hosts:hosts,matrices:[]}
+			for(h in hosts){
+				file="/home/brunocosta/Downloads/git/controlpanel/components/hosts/"+hosts[h]+"."+host+".log"
+				console.log(file)
+				result.matrices.push(data.getMatrix(file).then((res)=>{
 
-	//Convert Comments to functions
-	//iterDays
-	for (var i=0; i<days; i++){
-		let col = getColumn(numOfdates,days,i)
-		currentDate=datelist.pop()
-		labels.cols.unshift(res[currentDate].label)
-		delete res[currentDate].label
-		let keys=Object.keys(res[currentDate])
+					var datelist=Object.keys(res).sort()
+					var numOfdates=datelist.length
+					var matrix={}
+					matrix.data=generateMatrix(days,hours)
+					labels={
+					  	rows:[],
+					  	cols:[]
+					}
 
-		//iterHours
-		for (j in keys){
-			let key=keys[j]
-			let row=parseInt(key)
-			//Add values to matrix
-			if (res[currentDate][key]=="200"){
-				color="green"
-			}else{
-				color="red"
+					//Convert Comments to functions
+					//iterDays
+					for (var i=0; i<days; i++){
+						let col = getColumn(numOfdates,days,i)
+						currentDate=datelist.pop()
+						labels.cols.unshift(res[currentDate].label)
+						delete res[currentDate].label
+						let keys=Object.keys(res[currentDate])
+
+						//iterHours
+						for (j in keys){
+							let key=keys[j]
+							let row=parseInt(key)
+							//Add values to matrix
+							if (res[currentDate][key]=="200"){
+								color="green"
+							}else{
+								color="red"
+							}
+							matrix.data[row][col]={
+								color:color,
+								title:res[currentDate][key]
+							}
+						}
+
+						if((i+1) >= numOfdates ) break;
+					}
+					
+					matrix.labels=organizeLabels(labels,days,hours)
+					return matrix
+				}))
 			}
-			matrix.data[row][col]={
-				color:color,
-				title:res[currentDate][key]
-			}
+			resolve(result)
+		}catch(err){
+			reject(err)
 		}
-
-		if((i+1) >= numOfdates ) break;
-	}
-	
-	matrix.labels=organizeLabels(labels,days,hours)
-	return matrix
-})
+	})
+}
 
 function organizeLabels(labels,cols,rows){
 	if(labels.cols.length<cols){
@@ -76,4 +94,12 @@ function getColumn(arraySize,limit,iter){
 		//Can generate negative numbers
 		return arraySize-1-iter
 	}
+}
+
+function getHostNames(){
+	let hosts=glob.sync('components/hosts/*.json')
+	for(i in hosts){
+		hosts[i]=path.basename(hosts[i],'.json')
+	}
+	return hosts
 }
