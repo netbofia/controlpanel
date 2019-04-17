630b4c53f6fbf3197dfc16b05fdd48e0e85de1dc
Bruno Costa <netbofia@hotmail.com>
2019-04-17 15:34:22 +0100

    Adds the telegram controller that deals with the restart requests from bot

---
+++ components/controller/telegramController.js
@@ -0,0 +1,45 @@
+var telegram = require('./../helpers/telegram')
+var execCommands = require('./../helpers/execCommands')
+
+var servers={}
+var callback={}
+
+telegram.listen(servers,callback).then(function(res){
+	if(res.action){
+		console.log(res)
+		restartServer(res.server)
+	}
+	
+}).catch(function(err){
+
+})
+
+
+function callback(){
+
+}
+
+
+
+function restartServer(host){
+	return Promise(function(res,rej){
+
+		var command="ssh "+host+" sudo restartWebServer"
+		execCommands(command, callback)
+	
+		function callback(err, stdout, stderr){
+			if (err) rej(err)
+			if (stderr) rej(stderr)
+			res(stdout)
+		}
+	})
+
+}
+
+
+
+
+module.exports={
+
+}
+
