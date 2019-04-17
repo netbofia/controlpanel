9155a97b092dfa86247ec92d7adbdca6b9c3ba45
Bruno Costa <netbofia@hotmail.com>
2019-04-17 15:36:05 +0100

    Adds call to start listening to bot to restart server

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var data = require('./../components/parsing/buildHeatmapMatrix')
+
+//This is called on start server so its ok but if not used here maybe should pass on to app.js
+var telegramController = require('./../components/controller/telegramController')
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
  
   data.matrices().then((result)=>{
   	Promise.all(result.matrices).then(function(values){
+  		
     	res.render('index', { title: 'Control Panel',titles:result.hosts, matrices: values });
   	})
   }).catch(function(err){
