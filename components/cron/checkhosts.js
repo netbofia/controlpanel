2d2f3c1392e468f9bef7ff56c154801098cb14b1
Bruno Costa <netbofia@hotmail.com>
2019-04-17 15:34:55 +0100

    Simplied checkhost file

--- components/cron/checkhosts.js
+++ components/cron/checkhosts.js
@@ -1,80 +1,19 @@
 #!/usr/bin/env node
 
 //Cron Job Script to check if servers are up.
-
-
-var fs = require('fs');
 var glob = require('glob');
-var axios = require('axios');
-var expectedResponse="yes";
-
+var ghp=require('./../helpers/get_host_params')
+var writeLog=require('./../helpers/log_data').writeLog
+var expectedResponse="yes"; //on server
 
 //Possible problem in path
 var hosts=glob.sync(__dirname+'/../hosts/*.json')
-var parameters=getParameters(hosts)
-getAllResponses(parameters)
+var parameters=ghp.getHostParameters(hosts)
+ghp.getAllHostResponses(parameters,callback)
 
-
-
-function getParameters(hosts){
-	var result=[]
-	for (i in hosts){
-		result.push(require(hosts[i]))
-	}
-	return result
-}
-
-
-function getAllResponses(parameters){
-	var promises=[]
-	for (i in parameters){
-		let param=parameters[i]
-		let host=param.host;
-		let path=param.path;
-		let scheme=param.scheme;
-		let port=param.port;
-		promises.push(axios(scheme+"://"+host+"/"+path))
-	}
-	Promise.all(promises).then(function(values){
-		for (i in values){
-			let host=values[i].request.socket._host 
-			let response=values[i].data
-			let code=values[i].status
-			//Choose one
-			response==expectedResponse ? writeLog(host,code) : writeLog(host,"UNKNOWN_ERROR")
-			//code==200 ? writeLog(host,code) : writeLog(host,"UNKNOWN_ERROR")
-		}
-
-	}).catch(function(err){
-		let host=err.request._options.hostname
-		let response=err.code
-		notifyFailure(host,response)
-		writeLog(host,response)
-		testOtherServers(parameters,host)
-	})
-}
-
-function writeLog(host,res){
-	var iso = new Date()
-	var now = Date.now()
-	
-	let output=iso+"\t"+now+"\t"+host+"\t"+res
-	fs.appendFile(__dirname+'/../hosts/'+host+'.log', output+"\n", function (err) {
-	  if (err) throw err;
-	  console.log('Saved!: '+output);
-	});
-}
-
-function testOtherServers(parameters,host){
-		for (i in parameters){
-			if (parameters[i].host==host)
-				parameters.splice(i,1)
-		}
-		getAllResponses(parameters)
-}
-
-function notifyFailure(host,res){
-	let telegram=require('./sendtelegram')
-	msg=host+" | Problem detected: "+res
-	telegram.sendMSG(msg)
+function callback(hostResponse){
+	let response = hostResponse.response
+	let code = hostResponse.code
+	let host = hostResponse.host
+	response==expectedResponse ? writeLog(host,code) : writeLog(host,code)
 }
