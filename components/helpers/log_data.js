b5b02101cb3c37bdae1fe91d89fe6a811cd67c37
Bruno Costa <brunocosta@itqb.unl.pt>
2019-10-31 17:29:58 +0000

    Log state if resp is object

--- components/helpers/log_data.js
+++ components/helpers/log_data.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 
 function writeLog(hostResponse,parseResponse){
-	let response = parseResponse ? hostResponse.response.state : hostResponse.code
+	let logInfo = typeof hostResponse == "object" ? hostResponse.response.state : hostResponse.code
 	let code = hostResponse.code
 	let host = hostResponse.host
 	let hostFileName = hostResponse.hostFileName
 	var iso = new Date()
 	var now = Date.now()
 	
-	let output=iso+"\t"+now+"\t"+host+"\t"+response
+	let output=iso+"\t"+now+"\t"+host+"\t"+logInfo
 	fs.appendFile(__dirname+'/../hosts/'+hostFileName+'.log', output+"\n", function (err) {
 	  if (err) throw err;
 	  console.log('Saved!: '+output);
